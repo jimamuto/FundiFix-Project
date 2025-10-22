2e655dc301312aee7132ddf2f185b8e423f3254d
jim amuto <jim.amuto@strathmore.edu>
2025-10-23 01:18:09 +0300

    removed explicit credentials

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -3,19 +3,31 @@ namespace App\Config;
 
 use PDO;
 use PDOException;
+use Dotenv\Dotenv;
 
 class Database {
-    private $host = "localhost";
-    private $dbname = "fundi_database";
-    private $username = "root";
-    private $password = "rootuser";
-    private $port = 3310; 
+    private $host;
+    private $dbname;
+    private $username;
+    private $password;
+    private $port;
     public $conn;
+
+    public function __construct() {
+        // Load environment variables from .env
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+
+        $this->host = $_ENV['DB_HOST'];
+        $this->dbname = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASS'];
+        $this->port = $_ENV['DB_PORT'];
+    }
 
     public function connect() {
         $this->conn = null;
         try {
-        
             $dsn = "mysql:host={$this->host};port={$this->port};dbname={$this->dbname}";
             $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
